8105cfc6ffbcc3afe349eda51b513b633ad3056f
Troy Davidson <clandaith@gmail.com>
2023-07-14 14:52:10 -0600

    Added CrossOrigin

--- src/main/java/com/clandaith/wawsa/CardController.java
+++ src/main/java/com/clandaith/wawsa/CardController.java
@@ -1,6 +1,7 @@
 package com.clandaith.wawsa;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ public class CardController {
 	@Autowired
 	CardService cardService;
 
+	@CrossOrigin
 	@GetMapping("/api/card")
 	public Card card(@RequestParam(value = "saType", defaultValue = "") String saType) {
 		return cardService.getCard(saType);
